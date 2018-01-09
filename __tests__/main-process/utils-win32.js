abd1f7bf56087dd699b2af9a7ade8d1a7e10d234
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2018-01-09 17:01:42 -0500

    Use explorer.exe instead of cmd.exe to open directories. (Fixed #25)

--- __tests__/main-process/utils-win32.js
+++ __tests__/main-process/utils-win32.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Junpei Kawamoto
+ * Copyright (C) 2017-2018 Junpei Kawamoto
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ describe("utils module in Windows", () => {
     it("open a given directory with explorer", () => {
       const dir = "/tmp/some-dir";
       utils.openDirectory(dir);
-      expect(spawnSync).toHaveBeenCalledWith("cmd.exe", ["/c", "start", `"${dir}"`]);
+      expect(spawnSync).toHaveBeenCalledWith("explorer.exe", [dir]);
     });
 
   });
