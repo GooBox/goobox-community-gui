49f2be56435f64abd5865bed466d1727955bd68a
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2019-03-19 11:35:22 -0400

    chore: configure logs

--- src/render/popup/main.jsx
+++ src/render/popup/main.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 Junpei Kawamoto
+ * Copyright (C) 2017-2019 Junpei Kawamoto
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
